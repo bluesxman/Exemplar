90dafed79a30a1f1186e1b44f015bbd3b75fba8d
Jon Newton <jon.newton@gmail.com>
2022-02-05 10:29:50 -0700

    use defaultRequest

--- app/src/main/java/com/example/exemplar/client/spotify/SpotifyClient.kt
+++ app/src/main/java/com/example/exemplar/client/spotify/SpotifyClient.kt
@@ -2,6 +2,7 @@ package com.example.exemplar.client.spotify
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
@@ -11,11 +12,16 @@ val client = HttpClient(CIO) {
     install(JsonFeature) {
         serializer = KotlinxSerializer()
     }
+    defaultRequest {
+        host = "api.spotify.com"
+        header(HttpHeaders.Authorization, "Bearer ${System.getenv()["SPOTIFY_TOKEN"]}")
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+        header(HttpHeaders.Accept, ContentType.Application.Json)
+    }
 }
 
 suspend fun searchAlbums(query: String): SearchResponse {
-    return client.get("https://api.spotify.com/v1/search") {
-        header(HttpHeaders.Authorization, "Bearer ${System.getenv()["SPOTIFY_TOKEN"]}")
+    return client.get(path = "/v1/search") {
         parameter("q", query)
         parameter("type", "album")
         parameter("include_external", "audio")
