fab103f24354f0ed0fafc5eaaf3d53bf1535e2c6
Jon Newton <jon.newton@gmail.com>
2022-02-10 18:16:05 -0700

    add paging lib; fix no next in search response

--- app/src/main/java/com/example/exemplar/client/spotify/SearchResponse.kt
+++ app/src/main/java/com/example/exemplar/client/spotify/SearchResponse.kt
@@ -12,7 +12,7 @@ data class Albums(
     val href: String,
     val items: List<Item>,
     val limit: Int,
-    val next: String,
+    val next: String?,
     val offset: Int,
     val previous: String?,
     val total: Int
--- app/src/main/java/com/example/exemplar/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/exemplar/ui/search/SearchScreen.kt
@@ -1,10 +1,7 @@
 package com.example.exemplar.ui.search
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
 import androidx.compose.material.OutlinedTextField
@@ -14,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
@@ -55,7 +53,8 @@ fun SearchResultsContent(items: List<Item>) {
     LazyColumn(
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(colorResource(R.color.purple_200)),
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         items(items.size) { index ->
@@ -66,7 +65,7 @@ fun SearchResultsContent(items: List<Item>) {
 
 @Composable
 fun SearchItemContent(item: Item) {
-    Card {
+    Card(modifier = Modifier.fillMaxWidth().shadow(4.dp).height(100.dp)) {
         Text(item.name, fontSize = 30.sp)
     }
 }
